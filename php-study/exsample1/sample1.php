4d344cd22505f003e9bb3cfca7a53ed8b78fb542
TAKAKI SUGITANI <sugitani@ako-s.jp>
2016-06-20 11:41:55 +0900

    ぱっち

--- php-study/exsample1/sample1.php
+++ php-study/exsample1/sample1.php
@@ -3,7 +3,7 @@ require ('testFunctionInclude1.php');
 require ('testFunctionInclude2.php');
 require ('testFunctionInclude3.php');
 
-echo testFnPiyo( 7, 8 );
+echo "ANSWER=".testFnPiyo( 7, 8 ).PHP_EOL;
 
 
 
