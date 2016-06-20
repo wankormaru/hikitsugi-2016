d72530507e25fd9c5784a41ec9ee827259ea0a79
wankoromaru <sugitani@ako-s.jp>
2016-06-20 16:48:07 +0900

    Create clsPrius.php

---
+++ php-study/exsample3/app/classes/clsPriusclass-Prius-private-mileage-0/public-function-drive-distance/this--mileage-distance/clsPrius.php
@@ -0,0 +1,17 @@
+class Prius
+{
+    private $mileage = 0; // 走行距離
+
+    // 走行する
+    public function drive($distance)
+    {
+        // 走った距離分、走行距離を加算
+        $this->mileage += $distance;
+    }
+
+    // 走行距離を取得
+    public function getMileage()
+    {
+        return $this->mileage;
+    }
+}
